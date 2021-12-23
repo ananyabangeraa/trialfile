ad28f8c3a33db13a2e215b439c9799f383437718
Ananya Bangera <42906613+ananyabangeraa@users.noreply.github.com>
2021-12-23 11:00:12 +0530

    Easy reversing!

--- stringreverse.py
+++ stringreverse.py
@@ -1,7 +1,8 @@
 lep = "banana"
-new = ''
-n = 1
-while(n<=len(lep)):
-  new = new + lep[len(lep)-n]
-  n+=1
-print(new)
+# new = ''
+# n = 1
+print(lep[::-1])
+# while(n<=len(lep)):
+#   new = new + lep[len(lep)-n]
+#   n+=1
+# print(new)
