d742093f0851a1549324b5f8630014e864882b8d
Ananya Bangera <42906613+ananyabangeraa@users.noreply.github.com>
2021-12-23 11:15:02 +0530

    Added for loop example too!

--- stringreverse.py
+++ stringreverse.py
@@ -1,8 +1,14 @@
 lep = "banana"
-# new = ''
-# n = 1
+new = ''
+new1 = ''
+n = 1
 print(lep[::-1])
-# while(n<=len(lep)):
-#   new = new + lep[len(lep)-n]
-#   n+=1
-# print(new)
+# with while loop
+while(n<=len(lep)):
+  new = new + lep[len(lep)-n]
+  n+=1
+print(new)
+# with for loop
+for n in range(len(lep)):
+  new1 = new1 + lep[len(lep)-1-n]
+print(new1)
